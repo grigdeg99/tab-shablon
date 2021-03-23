05e98cfa1f4b554cd12d9c27afab22be52292485
Alex <example@gmail.com>
2021-03-23 09:15:24 +0300

    form

--- script.js
+++ script.js
@@ -42,8 +42,9 @@ window.addEventListener("DOMContentLoaded", function () {
 
 
 
+
 	//timer 
-	let deadline = "2021-03-20";
+	let deadline = "2021-03-23";
 
 	function setTimeRemaining(endtime) {
 		let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -97,6 +98,196 @@ window.addEventListener("DOMContentLoaded", function () {
 	setTimeValue('timer-numbers', deadline);
 
 
+	//pop up
+
+	let more = document.querySelector(".more"),
+		popUpContent = document.querySelector(".overlay"),
+		exitPoup = document.querySelector(".popup-close"),
+		tabbtn = document.querySelector(".description-btn");
+
+	more.addEventListener("click", function () {
+		popUpContent.style.display = "block";
+		this.classList.add("more-splash");
+		document.body.style.overflow = "hidden";
+
+	});
+
+	tabbtn.addEventListener("click", function () {
+		popUpContent.style.display = "block";
+		this.classList.add("more-splash");
+		document.body.style.overflow = "hidden";
+
+	});
+
+	exitPoup.addEventListener("click", function () {
+		popUpContent.style.display = "none";
+		more.classList.remove("more-splash");
+		document.body.style.overflow = "";
+		tabbtn.classList.remove("more-splash");
+	});
+
+	// Form
+
+	// let message = {
+	// 	loading: 'Загрузка',
+	// 	success: 'Спасибо! Скоро мы  с Вами свяжемся',
+	// 	failure: 'Что-то пошло не так '
+	// };
+
+	// let form = document.querySelector('.main-form'),
+	// 	input = form.getElementsByTagName('input'),
+	// 	statusMessage = document.createElement('div');
+
+	// statusMessage.classList.add('status');
+
+	// form.addEventListener('submit', function (event) {
+	// 	event.preventDefault();
+	// 	form.appendChild(statusMessage);
+	// 	let formData = new FormData(form);
+
+	// 	let request = new XMLHttpRequest();
+
+	// 	let obj = {};
+	// 	formData.forEach(function (value, key) {
+	// 		obj[key] = value;
+	// 	});
+
+	// 	let json = JSON.stringify(obj);
+
+
+	// 	request.open('POST', 'server.php');
+	// 	request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+	// 	request.send(json);
+	// 	request.addEventListener('readystatechange', function () {
+	// 		if (request.readyState < 4) {
+	// 			statusMessage.innerHTML = message.loading;
+	// 		} else if (request.readyState === 4 && request.status == 200) {
+	// 			statusMessage.innerHTML = message.success;
+	// 		} else {
+	// 			statusMessage.innerHTML = message.failure;
+	// 		}
+
+	// 	});
+
+	// 	for (let i = 0; i < input.length; i++) {
+	// 		input[i].value = '';
+	// 	}
+
+	// })
+
+
+	// Form Pop up
+
+	let messages = {
+		loading: 'Ожидайте, идет загрузка',
+		success: 'Прекрасно, спасибо за вам',
+		fail: 'Произошла ошибка'
+	};
+
+
+	let popForm = document.querySelector(".popup-form"),
+		form = popForm.querySelector('.main-form'),
+		input = document.querySelector('.popup-form__input'),
+		statusMessage = document.createElement('div');
+
+	statusMessage.classList.add('status');
+
+
+
+
+
+	function sendData(formed) {
+		formed.addEventListener('submit', function (event) {
+			event.preventDefault();
+			formed.appendChild(statusMessage);
+			let formData = new FormData(formed);
+
+			function postData(type, url) {
+				return new Promise((resolve, reject) => {
+					let request = new XMLHttpRequest();
+					request.open('POST', 'server.php');
+					request.setRequestHeader('Content-Type', url);
+					request.send(type);
+					request.addEventListener('readystatechange', function () {
+						if (request.readyState < 4) {
+							resolve();
+
+						} else if (request.readyState === 4 && request.status == 200) {
+							resolve();
+						} else {
+							reject();
+						}
+
+					});
+				})
+
+
+
+
+			}
+
+			function clearInput() {
+				input.value = '';
+			}
+
+			postData(formData, 'application/json; charset=utf-8')
+				.then(() => statusMessage.innerHTML = messages.loading)
+				.then(() => statusMessage.innerHTML = messages.success)
+				.catch(() => statusMessage.innerHTML = messages.fail)
+				.then(clearInput)
+
+		});
+	}
+
+	sendData(form);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
+
+let drink = 1;
+
+function shoot(arrow) {
+	console.log('Вы сделали выстрел...');
+	let promise = new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			Math.random() > .5 ? resolve({}) : reject("Вы промахнулись");
+
+		}, 3000);
+	});
+	return promise;
+
+}
+
+function win() {
+	console.log('Вы победили');
+	(drink == 1) ? buyBeer(): giveMoney();
+}
+
+function buyBeer() {
+	console.log('Вам купили пива');
+}
+
+function giveMoney() {
+	console.log('Вам заплатитли');
+}
+
+function loose() {
+	console.log('Вы проииграли');
+}
+
+shoot({})
+	.then(mark => console.log('Вы попали в цель'))
+	.then(win)
+	.catch(loose)
